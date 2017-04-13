7f6a4623d4179b2d549fed7fd24aa4dc44ba91a0
Kyle Behiels <Kyle Behiels>
2017-04-13 11:21:52 -0700

    Almost done

---
+++ gallery.js
@@ -0,0 +1,45 @@
+document.getElementById("sonora_from_water").onclick = function(){
+	document.getElementById("sonora_from_water").classList.toggle('fullScreen');
+}
+document.getElementById("model_unit").onclick = function(){
+	document.getElementById("model_unit").classList.toggle('fullScreen');
+}
+document.getElementById("toward_waterfront").onclick = function(){
+	document.getElementById("toward_waterfront").classList.toggle('fullScreen');
+}
+document.getElementById("living_room").onclick = function(){
+	document.getElementById("living_room").classList.toggle('fullScreen');
+}
+document.getElementById("waterfront").onclick = function(){
+	document.getElementById("waterfront").classList.toggle('fullScreen');
+}
+document.getElementById("across_field").onclick = function(){
+	document.getElementById("across_field").classList.toggle('fullScreen');
+}
+document.getElementById("2_flr_view").onclick = function(){
+	document.getElementById("2_flr_view").classList.toggle('fullScreen');
+}
+document.getElementById("model_kitchen").onclick = function(){
+	document.getElementById("model_kitchen").classList.toggle('fullScreen');
+}
+document.getElementById("another_water_view").onclick = function(){
+	document.getElementById("another_water_view").classList.toggle('fullScreen');
+}
+document.getElementById("condo_NE").onclick = function(){
+	document.getElementById("condo_NE").classList.toggle('fullScreen');
+}
+document.getElementById("dock").onclick = function(){
+	document.getElementById("dock").classList.toggle('fullScreen');
+}
+document.getElementById("dock2").onclick = function(){
+	document.getElementById("dock2").classList.toggle('fullScreen');
+}
+document.getElementById("sonora_south_street").onclick = function(){
+	document.getElementById("sonora_south_street").classList.toggle('fullScreen');
+}
+document.getElementById("south_lot_waterfront").onclick = function(){
+	document.getElementById("south_lot_waterfront").classList.toggle('fullScreen');
+}
+document.getElementById("south_lot").onclick = function(){
+	document.getElementById("south_lot").classList.toggle('fullScreen');
+}
